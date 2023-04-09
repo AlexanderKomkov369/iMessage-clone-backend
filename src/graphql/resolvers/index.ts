1c430d28fd50ff6f55c86d4ae654b18debc90278
Alexander Komkov <komkov.aleksandr@noorsoft.ru>
2023-04-09 18:59:04 +0300

    added scalars resolvers

--- src/graphql/resolvers/index.ts
+++ src/graphql/resolvers/index.ts
@@ -3,12 +3,14 @@ import userResolvers from "./user";
 import conversationResolvers from "./conversation";
 import messageResolvers from "./message";
 import merge from "lodash.merge";
+import scalarsResolvers from "./scalars";
 
 const resolvers: IExecutableSchemaDefinition["resolvers"] = merge(
   {},
   userResolvers,
   conversationResolvers,
-  messageResolvers
+  messageResolvers,
+  scalarsResolvers
 );
 
 export default resolvers;
